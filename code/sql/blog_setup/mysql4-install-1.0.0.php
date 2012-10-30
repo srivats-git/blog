eb858b4de6236a2b696b0375ee0b8f333f522d6b
Srivats <srivats@envisage-technologies.com>
2012-10-30 16:37:04 +0530

    Added table fields

--- code/sql/blog_setup/mysql4-install-1.0.0.php
+++ code/sql/blog_setup/mysql4-install-1.0.0.php
@@ -5,4 +5,19 @@ $installer=$this;
 $installer->startSetup();
 
 
+$installer->run("
+	CREATE TABLE IF NOT EXISTS `{$installer->getTable('blog/posts')}`
+	(
+		`id` smallint(6) NOT NULL AUTO_INCREMENT,
+		`title` varchar(255) NULL,
+		`identifier` varchar(100) NOT NULL,
+		`short_description` mediumtext NULL,
+		`description` mediumtext NULL,
+		`featured_image` varchar(255) NULL,
+		`creation_time` timestamp NULL,
+		`update_time` timestamp NULL,
+		PRIMARY KEY (`id`)
+	);
+");
+
 $installer->endSetup();
